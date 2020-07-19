8efb72a8a4f6efbc8d8144ad39a63c8b03e78b0e
Vlad <vladzhukovtirko@gmail.com>
2020-07-19 20:57:38 +0300

    up

---
+++ hex/links.js
@@ -0,0 +1,21 @@
+// called from rust
+function redirect_twitter() {
+    window.open( 
+        "https://twitter.com/VladZhukov0", "_blank"); 
+}
+
+register_plugin = function (importObject) {
+    importObject.env.redirect_twitter = redirect_twitter;
+}
+
+on_init = function () {
+    exports = wasm_exports;
+    memory = wasm_memory;
+    init();
+}
+
+init = function () {
+    console.log("wasm init finish");
+}
+
+miniquad_add_plugin({register_plugin, on_init});
